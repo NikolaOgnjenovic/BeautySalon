42a4859fd014d786fcdf014be4956fa3d93dc9c1
Sibirski plavac <nikola.31023@gmail.com>
2023-04-26 20:40:35 +0200

    [GUI] Added MiGLayout, changed welcome & login frames

--- src/com/mrmi/beautysalon/main/run/GUIApp.java
+++ src/com/mrmi/beautysalon/main/run/GUIApp.java
@@ -1,9 +1,11 @@
 package com.mrmi.beautysalon.main.run;
 
 import com.mrmi.beautysalon.main.view.MainFrame;
+import com.mrmi.beautysalon.main.view.modern.Welcome;
 
 public class GUIApp {
     public void run() {
-        MainFrame mainFrame = new MainFrame();
+//        MainFrame mainFrame = new MainFrame();
+        Welcome welcome = new Welcome();
     }
 }
--- src/com/mrmi/beautysalon/main/view/LoginFrame.java
+++ src/com/mrmi/beautysalon/main/view/LoginFrame.java
@@ -4,6 +4,7 @@ import com.mrmi.beautysalon.main.controller.AuthController;
 import com.mrmi.beautysalon.main.controller.TreatmentController;
 import com.mrmi.beautysalon.main.controller.UserController;
 import com.mrmi.beautysalon.main.entity.*;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,6 +16,7 @@ public class LoginFrame extends JFrame {
     private final AuthController authController;
     private JTextField username;
     private JPasswordField password;
+    private JLabel failedLoginLabel;
     private JButton login;
     public LoginFrame(TreatmentController treatmentController, UserController userController, BeautySalon beautySalon, AuthController authController) {
         this.treatmentController = treatmentController;
@@ -27,34 +29,48 @@ public class LoginFrame extends JFrame {
     }
 
     private void initialiseViews() {
-        this.setTitle("Login");
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.setResizable(true);
+        this.setLayout(new MigLayout("wrap 2", "[grow][grow]", "[grow]40[grow]40[grow]10[grow]"));
+        this.setTitle("Beauty salon - Login");
         this.setSize(800, 800);
-        this.setLayout(new FlowLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.getContentPane().setBackground(new Color(235, 235, 235));
 
-        username = new JTextField();
-        username.setToolTipText("Username");
+        JLabel title = new JLabel("Login to your account");
+        setFont(title, 30);
+        this.add(title, "span, align center");
+
+        JLabel usernameLabel = new JLabel("Username:");
+        setFont(usernameLabel, 24);
+        this.add(usernameLabel, "align right");
+
+        username = new JTextField(18);
+        setFont(username, 24);
         username.setPreferredSize(new Dimension(250, 40));
-        this.add(username);
-        System.out.println("u len: " + username.getText().length());
+        this.add(username, "align left");
 
-        password = new JPasswordField();
-        password.setToolTipText("Password");
+        JLabel passwordLabel = new JLabel("Password:");
+        setFont(passwordLabel, 24);
+        this.add(passwordLabel, "align right");
+
+        password = new JPasswordField(18);
         password.setPreferredSize(new Dimension(250, 40));
-        this.add(password);
+        setFont(password, 24);
+        this.add(password, "align left");
 
         login = new JButton("Login");
-        this.add(login);
+        setFont(login, 24);
+        this.add(login, "span, align center");
+
+        failedLoginLabel = new JLabel(" ");
+        setFont(failedLoginLabel, 20);
+        failedLoginLabel.setForeground(Color.RED);
+        this.add(failedLoginLabel, "span, align center");
     }
 
     private void initialiseListeners() {
         login.addActionListener(e -> {
             if (!authController.login(username.getText(), String.valueOf(password.getPassword()))) {
-                username.setText("Wrong username");
-                password.setText("Wrong password");
+                failedLoginLabel.setText("Invalid credentials. Please try again!");
             } else {
                 if (authController.getCurrentUser().getClass().equals(Client.class)) {
                     this.dispose();
@@ -71,5 +87,9 @@ public class LoginFrame extends JFrame {
                 }
             }
         });
+    }
+
+    private void setFont(JComponent component, float size) {
+        component.setFont(component.getFont().deriveFont(size));
     }
 }
